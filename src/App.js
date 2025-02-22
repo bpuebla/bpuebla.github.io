e03c4f91638d9ba87c0e95afd0d7db308c8874c5
bpueblaXMP <0bautistapuebla@gmail.com>
2025-02-22 23:24:26 +0100

    Update routing in App component to use Routes; import components from the correct paths

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './components/HomePage';
-import AboutPage from './public/about.html'; // Placeholder for About component
-import ServicesPage from './public/services.html'; // Placeholder for Services component
-import ContactPage from './public/contact.html'; // Placeholder for Contact component
+import AboutPage from './components/AboutPage';
+import ServicesPage from './components/ServicesPage';
+import ContactPage from './components/ContactPage';
+import PostPage from './components/PostPage';
+import "./styles/home.css";
 
 const App = () => {
   return (
     <Router>
-      <Switch>
-        <Route path="/" exact component={HomePage} />
-        <Route path="/about" component={AboutPage} />
-        <Route path="/services" component={ServicesPage} />
-        <Route path="/contact" component={ContactPage} />
-      </Switch>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/services" element={<ServicesPage />} />
+        <Route path="/contact" element={<ContactPage />} />
+        <Route path="/:postId" element={<PostPage />} />
+      </Routes>
     </Router>
   );
 };
